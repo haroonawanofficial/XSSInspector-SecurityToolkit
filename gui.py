16931e43c3b82053730135a270c9d7545466889b
Haroon Awan <mrharoonawan@gmail.com>
2023-11-03 18:35:15 +0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,35 +1,70 @@
-import threading
 import sys
 import os
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QTextEdit, QLineEdit, QCheckBox, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QTextEdit, QLineEdit, QCheckBox, QFileDialog, QProgressBar
 from PyQt5.QtGui import QTextCharFormat, QColor
-from PyQt5.QtCore import QTimer, pyqtSlot
+from PyQt5.QtCore import QTimer, pyqtSlot, QUrl, QThread, Qt
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtGui import QTextCursor
-from PyQt5.QtCore import QUrl
-from PyQt5.QtCore import pyqtSignal, QObject
-from PyQt5.QtCore import Qt, QThread
-from PyQt5.QtGui import QDesktopServices
 from datetime import datetime
 
-# Create custom wrapper classes for QTextCursor and QTextCharFormat
-class CustomQTextCursor(QTextCursor):
-    pass
+class AnimatedProgressBar(QProgressBar):
+    def __init__(self, duration=1000, steps=1000):
+        super().__init__()
+        self.duration = duration
+        self.steps = steps
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_animation)
+        self.animation_value = 0
+        self.animation_direction = 1
+        self.is_running = False
 
-class CustomQTextCharFormat(QTextCharFormat):
-    pass
+        # Customize the appearance of the progress bar
+        self.setFixedWidth(100)  # Set the default width of the progress bar
+        self.setTextVisible(False)
+        self.setMinimum(0)
+        self.setMaximum(100)
+        self.setAlignment(Qt.AlignCenter)
+        self.setStyleSheet(
+            "QProgressBar { border: 2px solid grey; border-radius: 10px; background-color: #f0f0f0; } "
+            "QProgressBar::chunk { background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #49a2db, stop: 1 #105384); }"
+        )
 
+    def set_bar_width(self, bar_width):
+        self.setFixedWidth(bar_width)
+
+    def start_animation(self):
+        if not self.is_running:
+            self.is_running = True
+            self.timer.start(self.duration // self.steps)
+
+    def stop_animation(self):
+        self.is_running = False
+        self.timer.stop()
+
+    def update_animation(self):
+        if not self.is_running:
+            return
+
+        if self.animation_value == 0:
+            self.animation_direction = 1
+        elif self.animation_value == self.steps:
+            self.animation_direction = -1
+
+        self.animation_value += self.animation_direction
+        self.setValue(int((self.animation_value / self.steps) * 100))
+
+# Define the ScanThread class
 class ScanThread(QThread):
     update_signal = pyqtSignal(str)
+    progress_signal = pyqtSignal(int)
 
     def __init__(self, command):
         super().__init__()
         self.command = command
-        self.stopped = False  # Initialize a flag to indicate if the thread is stopped
-        self.is_running = False  # Add a flag to track if the thread is running
+        self.stopped = False
 
     def run(self):
-        self.is_running = True  # Thread is running
         try:
             process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
             for line in process.stdout:
@@ -37,14 +72,19 @@ class ScanThread(QThread):
                     break
                 self.update_signal.emit(line)
 
+                # Simulate progress (you should update this logic)
+                for i in range(101):
+                    if self.stopped:
+                        break
+                    self.progress_signal.emit(i)
+                    self.msleep(100)  # Sleep to simulate work
+
             process.wait()
         except Exception as e:
             self.update_signal.emit(f"Error: {str(e)}")
-        finally:
-            self.is_running = False  # Thread is no longer running
 
     def stop_scan(self):
-        self.stopped = True  # Set the flag to stop the thread
+        self.stopped = True
 
 class XSSInspectorApp(QWidget):
     def __init__(self):
@@ -52,21 +92,6 @@ class XSSInspectorApp(QWidget):
         self.initUI()
         self.scanning_thread = None
         os.environ['PYTHONUNBUFFERED'] = '1'
-        self.update_timer = QTimer()
-        self.update_timer.timeout.connect(self.process_updates)
-        self.pending_updates = []
-
-        # Register QTextCursor and QTextCharFormat using qRegisterMetaType
-        self.register_custom_types()
-
-    def register_custom_types(self):
-        text_cursor = QTextCursor(self.results_text.document())
-        char_format = text_cursor.charFormat()
-        
-    def process_updates(self):
-        if self.pending_updates:
-            self.update_results_text("".join(self.pending_updates))
-            self.pending_updates.clear()
 
     def initUI(self):
         self.setWindowTitle('XSS Inspector')
@@ -96,81 +121,94 @@ class XSSInspectorApp(QWidget):
         self.link_label = QLabel('<b>XSS Inspector</b> by Haroon Ahmad Awan | <a href="http://www.cyberzeus.pk">Cyberzeus.pk</a>', self)
         self.link_label.setOpenExternalLinks(True)
         self.link_label.linkActivated.connect(self.open_link)
+
+        # Create a layout for the progress bar and its label
+        progress_layout = QHBoxLayout()
+        self.progress_label = QLabel('Progress Bar:', self)
+        self.progress_bar = AnimatedProgressBar()
+        progress_layout.addWidget(self.progress_label)
+        progress_layout.addWidget(self.progress_bar)
+        
         layout = QVBoxLayout()
-        layout.addWidget(self.domain_label)
-        layout.addWidget(self.domain_input)
-        layout.addWidget(self.url_list_label)
-        layout.addWidget(self.url_list_input)
+        layout_domain = QHBoxLayout()
+        layout_domain.addWidget(self.domain_label)
+        layout_domain.addWidget(self.domain_input)
+        layout_url_list = QHBoxLayout()
+        layout_url_list.addWidget(self.url_list_label)
+        layout_url_list.addWidget(self.url_list_input)
+        layout.addLayout(layout_domain)
+        layout.addLayout(layout_url_list)
         layout.addWidget(self.browse_button)
         layout.addWidget(self.reports_checkbox)
         layout.addWidget(self.threads_checkbox)
         layout.addWidget(self.include_subdomain_checkbox)
         layout.addWidget(self.deep_crawl_checkbox)
+        layout.addLayout(progress_layout)  # Adding the progress_layout here
         layout.addWidget(self.results_text)
-        layout.addWidget(self.scan_button)
-        layout.addWidget(self.stop_button)
-        layout.addWidget(self.close_button)
+
+        layout_buttons = QHBoxLayout()
+        layout_buttons.addWidget(self.scan_button)
+        layout_buttons.addWidget(self.stop_button)
+        layout_buttons.addWidget(self.close_button)
+        layout.addLayout(layout_buttons)
+
         layout.addWidget(self.link_label)
         self.setLayout(layout)
-        # Set the initial message
-        self.set_text_color("Ready to scan.", QColor("darkGreen"))
 
     @pyqtSlot()
     def stop_scan(self):
-        if self.scanning_thread and self.scanning_thread.is_running:
+        if self.scanning_thread and self.scanning_thread.isRunning():
             self.scanning_thread.stop_scan()
             self.scan_button.setEnabled(True)
             self.stop_button.setEnabled(False)
+            self.progress_bar.stop_animation()
 
     @pyqtSlot()
     def scan_finished(self):
         self.results_text.append("Scan finished.")
         self.scan_button.setEnabled(True)
         self.stop_button.setEnabled(False)
+        self.progress_bar.stop_animation()
 
     def start_scan(self):
-        self.results_text.clear()
-        self.scan_button.setEnabled(False)
-        self.stop_button.setEnabled(True)
-        self.set_text_color("Initializing...", QColor("darkGreen"))
-        domain = self.domain_input.text()
-        url_list = self.url_list_input.text()
-        if not domain and not url_list:
-            self.set_text_color("Please enter a domain or select a URL list file.", QColor("red"))
-            self.scan_button.setEnabled(True)
-            self.stop_button.setEnabled(False)
-            return
-        command = ["python3", "xssinspector.py", "--domain", domain, "--list", url_list]
+        if not self.scanning_thread or not self.scanning_thread.isRunning():
+            self.results_text.clear()
+            self.scan_button.setEnabled(False)
+            self.stop_button.setEnabled(True)
+            self.set_text_color("Initializing...", QColor("darkGreen"))
+            domain = self.domain_input.text()
+            url_list = self.url_list_input.text()
+            if not domain and not url_list:
+                self.set_text_color("Please enter a domain or select a URL list file.", QColor("red"))
+                self.scan_button.setEnabled(True)
+                self.stop_button.setEnabled(False)
+                return
+            command = ["python3", "xssinspector.py", "--domain", domain, "--list", url_list]
 
-        # Create a thread to run xssinspector.py and capture output
-        self.scanning_thread = ScanThread(command)  # Create an instance of ScanThread
-        self.scanning_thread.update_signal.connect(self.update_results_text)  # Connect the signal
-        self.scanning_thread.start()
-        
-    def run_scan(self, command):
-        try:
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
-            for line in process.stdout:
-                self.update_results_text(line)
+            # Create a thread to run xssinspector.py and capture output
+            self.scanning_thread = ScanThread(command)
+            self.scanning_thread.update_signal.connect(self.update_results_text)
+            self.scanning_thread.progress_signal.connect(self.update_progress)
+            self.scanning_thread.start()
+            self.progress_bar.set_bar_width(200)  # Set the width of the progress bar
+            self.progress_bar.start_animation()
 
-            process.wait()
-            self.scan_finished()
-        except Exception as e:
-            self.update_results_text(f"Error: {str(e)}")
-            self.scan_finished()
+    @pyqtSlot(int)
+    def update_progress(self, progress_value):
+        self.progress_bar.setValue(progress_value)
 
     @pyqtSlot(str)
     def update_results_text(self, output):
         timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S] ")
         cursor = QTextCursor(self.results_text.document())
         cursor.movePosition(QTextCursor.End)
-        cursor.insertText(timestamp, QTextCharFormat())  # Use QTextCharFormat directly
+        cursor.insertText(timestamp, QTextCharFormat())
         cursor.insertText(output)
         self.results_text.setTextCursor(cursor)
         self.results_text.verticalScrollBar().setValue(self.results_text.verticalScrollBar().maximum())
 
     def close_app(self):
-        if self.scanning_thread and self.scanning_thread.stopped:
+        if self.scanning_thread and self.scanning_thread.isRunning():
             self.scanning_thread.stop_scan()
         self.close()
 
